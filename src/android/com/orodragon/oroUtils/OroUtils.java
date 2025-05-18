80ebc7d121855c5e55c37ade77988bf708b4a7f6
oren-oro <88835671+oren-oro@users.noreply.github.com>
2025-05-18 15:44:13 +0300

    Create OroUtils.java

---
+++ src/android/com/orodragon/oroUtils/OroUtils.java
@@ -0,0 +1,35 @@
+package com.orodragon.oroUtils;
+
+import android.util.Log;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class OroUtils extends CordovaPlugin {
+
+    private static final String APPTAG = "OroUtils";
+
+    @Override
+    public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("setThreadName")) {
+            Log.i(APPTAG, "setThreadName");
+            cordova.getThreadPool().execute(() -> {
+                try {
+                    String threadName = args.getString(0);
+                    if (threadName == null || threadName.trim().length() == 0)
+                        throw new JSONException("Missing thread name!");
+                    Thread.currentThread().setName(threadName);
+                    callbackContext.success();
+                }
+                catch (JSONException exception) {
+                    callbackContext.error(exception.getMessage());
+                }
+            });
+            return true;
+        } else {
+            callbackContext.error("Method not allowed");
+            return false;
+        }
+    }
+}
